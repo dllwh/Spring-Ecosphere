978dcb8a96a0d6f3253f547103ef3e5f4ba11c02
独泪了无痕 <duleilewuhen@sina.com>
2018-11-25 10:48:38 +0800

    Spring Data for Apache Solr

---
+++ SpringBoot2-chapter11-apacheSolr/src/main/java/com/cdeledu/App.java
@@ -0,0 +1,13 @@
+package com.cdeledu;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
