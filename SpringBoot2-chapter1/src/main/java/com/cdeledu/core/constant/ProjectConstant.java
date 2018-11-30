1bae4d23f657f1f6665f64bda7d4c8fccbcdc758
独泪了无痕 <duleilewuhen@sina.com>
2018-11-30 23:51:05 +0800

    项目系统接口常量，为自动代码生成配置

---
+++ SpringBoot2-chapter1/src/main/java/com/cdeledu/core/constant/ProjectConstant.java
@@ -0,0 +1,28 @@
+package com.cdeledu.core.constant;
+
+/**
+ * 把今天最好的表现当作明天最新的起点．．～
+ *
+ * Today the best performance as tomorrow newest starter!
+ *
+ * @类描述: 项目系统接口常量，为自动代码生成配置
+ * @创建者: 皇族灬战狼
+ * @联系方式: duleilewuhen@sina.com
+ * @创建时间: 2018年11月29日 上午8:20:45
+ * @版本: V 0.1
+ * @since: JDK 1.8
+ */
+public class ProjectConstant {
+	// 项目基础包名称，根据自己公司的项目修改
+	public static final String BASE_PACKAGE = "com.cdeledu";
+	// Model所在包
+	public static final String MODEL_PACKAGE = BASE_PACKAGE + ".model";
+	// Mapper所在包
+	public static final String MAPPER_PACKAGE = BASE_PACKAGE + ".dao";
+	// Service所在包
+	public static final String SERVICE_PACKAGE = BASE_PACKAGE + ".service";
+	// ServiceImpl所在包
+	public static final String SERVICE_IMPL_PACKAGE = SERVICE_PACKAGE + ".impl";
+	// Controller所在包
+	public static final String CONTROLLER_PACKAGE = BASE_PACKAGE + ".controller";
+}
