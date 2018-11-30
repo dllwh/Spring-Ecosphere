1afb1d133bf0ab4ffde81599e1c50ab538553930
独泪了无痕 <duleilewuhen@sina.com>
2018-11-30 23:34:58 +0800

    项目启动类

---
+++ SpringBoot2-chapter11-apacheSolr/src/main/java/com/cdeledu/ApacheSolrSpringBootApplcation.java
@@ -0,0 +1,22 @@
+package com.cdeledu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 把今天最好的表现当作明天最新的起点．．～
+ *
+ * Today the best performance as tomorrow newest starter!
+ *
+ * @类描述: 项目启动类
+ * @创建者: 独泪了无痕--duleilewuhen@sina.com
+ * @创建时间: 2018年11月30日 下午11:23:46
+ * @版本: V1.0.1
+ * @since: JDK 1.8
+ */
+@SpringBootApplication
+public class ApacheSolrSpringBootApplcation {
+	public static void main(String[] args) {
+		SpringApplication.run(ApacheSolrSpringBootApplcation.class, args);
+	}
+}
---
+++ SpringBoot2-chapter8-amqp/src/main/java/com/cdeledu/AmqpSpringBootApplication.java
@@ -0,0 +1,22 @@
+package com.cdeledu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 把今天最好的表现当作明天最新的起点．．～
+ *
+ * Today the best performance as tomorrow newest starter!
+ *
+ * @类描述: 项目启动类
+ * @创建者: 独泪了无痕--duleilewuhen@sina.com
+ * @创建时间: 2018年11月30日 下午11:27:22
+ * @版本: V1.0.1
+ * @since: JDK 1.8
+ */
+@SpringBootApplication
+public class AmqpSpringBootApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(AmqpSpringBootApplication.class, args);
+	}
+}
