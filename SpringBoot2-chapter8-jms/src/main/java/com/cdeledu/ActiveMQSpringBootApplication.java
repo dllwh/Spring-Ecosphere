b3568fe0295ac71200405cd6e1669420873faad2
独泪了无痕 <duleilewuhen@sina.com>
2018-12-03 22:03:00 +0800

    异步消息JMS（ActiveMQ）

---
+++ SpringBoot2-chapter8-jms/src/main/java/com/cdeledu/ActiveMQSpringBootApplication.java
@@ -0,0 +1,22 @@
+package com.cdeledu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 把今天最好的表现当作明天最新的起点．．～
+ *
+ * Today the best performance as tomorrow newest starter!
+ *
+ * @类描述: ActiveMQ
+ * @创建者: 独泪了无痕--duleilewuhen@sina.com
+ * @创建时间: 2018年12月3日 下午10:01:14
+ * @版本: V1.0.1
+ * @since: JDK 1.8
+ */
+@SpringBootApplication
+public class ActiveMQSpringBootApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ActiveMQSpringBootApplication.class, args);
+	}
+}
