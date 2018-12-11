43cbc3c7f2faad7be4ae41251d9eed57a622e41b
独泪了无痕 <8423799+dllwh@users.noreply.github.com>
2018-12-11 15:11:46 +0800

    Swagger2 配置属性说明

--- SpringBoot2-chapter1/src/main/java/com/cdeledu/core/configuration/SwaggerConfig.java
+++ SpringBoot2-chapter1/src/main/java/com/cdeledu/core/configuration/SwaggerConfig.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
@@ -16,7 +18,7 @@ import springfox.documentation.spring.web.plugins.Docket;
  * Today the best performance as tomorrow newest starter!
  *
  * @类描述: Swagger2配置
- * 		
+ * 
  *       <pre>
  *       	1、通过@Configuration注解，让Spring来加载该类配置
  *       	2、通过@EnableSwagger2注解来启用Swagger2，在本示例项目中，为了统一管理，该注解放置于Application.java
@@ -24,7 +26,7 @@ import springfox.documentation.spring.web.plugins.Docket;
  * 
  * @创建者: 独泪了无痕--duleilewuhen@sina.com
  * @创建时间: 2018年10月18日 上午12:47:32
- * @版本: V1.0.3
+ * @版本: V1.0.4
  * @since: JDK 1.8
  */
 @Configuration
@@ -33,33 +35,48 @@ public class SwaggerConfig {
 	/**
 	 * @方法描述:swagger2的配置文件，这里可以配置swagger2的一些基本的内容
 	 * @return
-	 * 
-	 * @Api：修饰整个类，描述Controller的作用
-	 * @ApiOperation：描述一个类的一个方法，或者说一个接口
-	 * @ApiParam：单个参数描述
-	 * @ApiModel：用对象来接收参数
-	 * @ApiProperty：用对象接收参数时，描述对象的一个字段
-	 * @ApiResponse：HTTP响应其中1个描述
-	 * @ApiResponses：HTTP响应整体描述
-	 * @ApiIgnore：使用该注解忽略这个API
-	 * @ApiError：发生错误返回的信息
-	 * @ApiImplicitParam：一个请求参数
-	 * @ApiImplicitParams：多个请求参数
 	 */
 	@Bean
 	public Docket customDocket() {
-		return new Docket(DocumentationType.SWAGGER_2)
-				.select() // 选择那些路径和api会生成document
-				.build() // 创建
+		return new Docket(DocumentationType.SWAGGER_2).groupName("")// 设置栏目名
 				.apiInfo(apiInfo()) // 用来创建该Api的基本信息（这些基本信息会展现在文档页面中）
-		// .apis(RequestHandlerSelectors.withClassAnnotation(Api.class)
-		// .apis(RequestHandlerSelectors.withMethodAnnotation(Api.class))
-		// .apis(RequestHandlerSelectors.basePackage("com.cdeledu.controller"))
-		// .paths(PathSelectors.any())
-		// .build()
-		;
+				.pathMapping("/").select() // 选择那些路径和api会生成document
+				.build(); // 创建
 	}
-	
+
+	@Bean
+	public Docket alipayApi() {
+		return new Docket(DocumentationType.SWAGGER_2).groupName("支付宝API接口文档") // 设置栏目名
+				.apiInfo(apiInfo()) // 构建 api文档的详细信息函数
+				.pathMapping("/") // 设置api根路径
+				.select() // 初始化并返回一个API选择构造器
+				.apis(RequestHandlerSelectors.basePackage("com.cdeledu.project.alipay")) // 指定扫描的包路径
+				.paths(PathSelectors.any()) // 指定路径处理PathSelectors.any()代表所有的路径
+				.build(); // 创建
+	}
+
+	@Bean
+	public Docket weixinpayApi() {
+		return new Docket(DocumentationType.SWAGGER_2).groupName("微信API接口文档") // 设置栏目名
+				.apiInfo(apiInfo())// 构建 api文档的详细信息函数
+				.pathMapping("/")// 设置api根路径
+				.select() // 初始化并返回一个API选择构造器
+				.apis(RequestHandlerSelectors.basePackage("com.cdeledu.project.weixinpay"))// 指定扫描的包路径
+				.paths(PathSelectors.any()) // 指定路径处理PathSelectors.any()代表所有的路径
+				.build(); // 创建
+	}
+
+	@Bean
+	public Docket unionpayApi() {
+		return new Docket(DocumentationType.SWAGGER_2).groupName("银联API接口文档")// 设置栏目名
+				.apiInfo(apiInfo())// 构建 api文档的详细信息函数
+				.pathMapping("/")// 设置api根路径
+				.select() // 初始化并返回一个API选择构造器
+				.apis(RequestHandlerSelectors.basePackage("com.cdeledu.project.unionpay"))// 指定扫描的包路径
+				.paths(PathSelectors.any()) // 指定路径处理PathSelectors.any()代表所有的路径
+				.build(); // 创建
+	}
+
 	/**
 	 * @方法描述:构建 api文档的详细信息函数
 	 * @return
