933ca682a01231c73259e9e2991c9f3950c1fdf8
独泪了无痕 <duleilewuhen@sina.com>
2018-12-13 23:38:40 +0800

    Signed-off-by: 独泪了无痕 <duleilewuhen@sina.com>

--- SpringBoot2-chapter14-shiro/src/main/java/com/cdeledu/modules/monitor/job/domain/Job.java
+++ SpringBoot2-chapter14-shiro/src/main/java/com/cdeledu/modules/monitor/job/domain/Job.java
@@ -1,5 +1,9 @@
 package com.cdeledu.modules.monitor.job.domain;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
 /**
  * 把今天最好的表现当作明天最新的起点．．～
  *
@@ -11,6 +15,23 @@ package com.cdeledu.modules.monitor.job.domain;
  * @版本: V1.0.1
  * @since: JDK 1.8
  */
+@Data
+@ApiModel
 public class Job {
-
+	@ApiModelProperty(value = "任务ID", name = "id", example = "1")
+	private String	jobId;
+	@ApiModelProperty(value = "任务名称", name = "jobName")
+	private String	jobName;
+	@ApiModelProperty(value = "任务分组", name = "jobGroup")
+	private String	jobGroup;
+	@ApiModelProperty(value = "任务描述", name = "description")
+	private String	description;
+	@ApiModelProperty(value = "执行类", name = "methodName")
+	private String	methodName;
+	@ApiModelProperty(value = "执行表达式", name = "cronExpression")
+	private String	cronExpression;
+	@ApiModelProperty(value = "执行时间", name = "triggerName")
+	private String	triggerName;
+	@ApiModelProperty(value = "任务状态", name = "triggerState", example = "1")
+	private Integer	triggerState;
 }
