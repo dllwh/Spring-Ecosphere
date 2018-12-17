d1c9b2482525df7be20239212727525aa7896d68
独泪了无痕 <duleilewuhen@sina.com>
2018-12-17 23:03:38 +0800

    基于spring-boot 2.x + quartz 的CRUD任务管理系统。

---
+++ SpringBoot2-chapter15-task/src/main/java/org/dllwh/TaskSpringBootApplication.java
@@ -0,0 +1,11 @@
+package org.dllwh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskSpringBootApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(TaskSpringBootApplication.class, args);
+	}
+}
