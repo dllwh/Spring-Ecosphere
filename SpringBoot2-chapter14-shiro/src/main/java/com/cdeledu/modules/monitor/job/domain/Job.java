996e1629135706ac2985d9a9bbaf1de2b4aa8003
独泪了无痕 <duleilewuhen@sina.com>
2018-12-20 23:17:58 +0800

    定时任务

--- SpringBoot2-chapter14-shiro/src/main/java/com/cdeledu/modules/monitor/job/domain/Job.java
+++ SpringBoot2-chapter14-shiro/src/main/java/com/cdeledu/modules/monitor/job/domain/Job.java
@@ -1,5 +1,11 @@
 package com.cdeledu.modules.monitor.job.domain;
 
+import java.util.Date;
+
+import javax.validation.constraints.NotBlank;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
@@ -21,6 +27,7 @@ public class Job {
 	@ApiModelProperty(value = "任务ID", name = "id", example = "1")
 	private String	id;
 	@ApiModelProperty(value = "任务名称", name = "jobName")
+	@NotBlank(message="方法名称不能为空")
 	private String	jobName;
 	@ApiModelProperty(value = "任务分组", name = "jobGroup")
 	private String	jobGroup;
@@ -29,9 +36,11 @@ public class Job {
 	@ApiModelProperty(value = "执行类", name = "methodName")
 	private String	methodName;
 	@ApiModelProperty(value = "执行表达式", name = "cronExpression")
+	@NotBlank(message = "cron表达式不能为空")
 	private String	cronExpression;
 	@ApiModelProperty(value = "执行时间", name = "triggerName")
-	private String	triggerName;
+	@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+	private Date	triggerName;
 	@ApiModelProperty(value = "任务状态", name = "triggerState", example = "1")
 	private Integer	triggerState;
 }
