fecf82b0e32da7bc07280ae4f4efffb5f0d17ce1
独泪了无痕 <8423799+dllwh@users.noreply.github.com>
2018-12-27 14:42:02 +0800

    更新信息提示

--- SpringBoot2-chapter14-shiro/src/main/java/com/cdeledu/common/RestResult.java
+++ SpringBoot2-chapter14-shiro/src/main/java/com/cdeledu/common/RestResult.java
@@ -16,16 +16,16 @@ import lombok.Data;
  * @类描述: 统一返回数据结构
  * @创建者: 独泪了无痕--duleilewuhen@sina.com
  * @创建时间: 2018年12月12日 下午10:39:06
- * @版本: V1.0.1
+ * @版本: V1.0.2
  * @since: JDK 1.8
  */
 @Data
-@ApiModel(value = "RestResult",description="统一返回数据结构")
+@ApiModel(value = "RestResult", description = "统一返回数据结构")
 public class RestResult {
 	/** 服务器当前时间（添加该字段的原因是便于查找定位请求时间，因为实际开发过程中服务器时间可能跟本地时间不一致，加上这个时间戳便于日后定位） */
 	@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
 	private LocalDateTime	currentTime	= LocalDateTime.now();
-	@ApiModelProperty(value = "成功或者失败的code错误码", name = "statusCode")
+	@ApiModelProperty(value = "结果响应码", name = "statusCode")
 	private Integer			statusCode;
 	@ApiModelProperty(value = "访问Url", name = "url")
 	private String			url;
@@ -33,11 +33,11 @@ public class RestResult {
 	private String			error;
 	@ApiModelProperty(value = "错误的堆栈轨迹", name = "stackTrace")
 	private String			stackTrace;
-	@ApiModelProperty(value = "成功时返回的数据息", name = "data")
+	@ApiModelProperty(value = "返回的数据", name = "data")
 	private Object			data;
 	@ApiModelProperty(value = "请求失败返回的提示信息，给前端进行页面展示的信息", name = "message")
 	private String			message;
-							
+
 	/**
 	 * @方法描述:返回错误消息
 	 * @return
@@ -45,7 +45,7 @@ public class RestResult {
 	public static RestResult error() {
 		return error(500, "操作失败");
 	}
-	
+
 	/**
 	 * @方法描述:返回错误消息
 	 * @param message
@@ -54,7 +54,7 @@ public class RestResult {
 	public static RestResult error(String message) {
 		return error(500, message);
 	}
-	
+
 	/**
 	 * @方法描述:返回错误消息
 	 * @param statusCode
@@ -67,11 +67,11 @@ public class RestResult {
 		restResult.setMessage(message);
 		return restResult;
 	}
-	
+
 	public static RestResult success() {
 		return success("操作成功");
 	}
-	
+
 	public static RestResult success(String message) {
 		RestResult restResult = new RestResult();
 		restResult.setStatusCode(200);
