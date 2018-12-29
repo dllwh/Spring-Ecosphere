8a429f36e1a8ee40787d202cfd96ee91b4c88cfd
独泪了无痕 <8423799+dllwh@users.noreply.github.com>
2018-12-29 11:54:31 +0800

    自定义全局异常处理器 V1.0.4

--- SpringBoot2-chapter14-shiro/src/main/java/com/cdeledu/framework/exception/DefaultExceptionHandler.java
+++ SpringBoot2-chapter14-shiro/src/main/java/com/cdeledu/framework/exception/DefaultExceptionHandler.java
@@ -1,5 +1,9 @@
 package com.cdeledu.framework.exception;
 
+import org.apache.shiro.authc.DisabledAccountException;
+import org.apache.shiro.authc.IncorrectCredentialsException;
+import org.apache.shiro.authc.LockedAccountException;
+import org.apache.shiro.authc.UnknownAccountException;
 import org.apache.shiro.authz.AuthorizationException;
 import org.springframework.beans.ConversionNotSupportedException;
 import org.springframework.http.converter.HttpMessageNotReadableException;
@@ -12,7 +16,6 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import com.cdeledu.common.RestResult;
-import com.cdeledu.common.exception.user.UserNotExistsException;
 import com.cdeledu.common.util.SystemLogHelper;
 
 import lombok.extern.slf4j.Slf4j;
@@ -25,20 +28,60 @@ import lombok.extern.slf4j.Slf4j;
  * @类描述: 自定义全局异常处理器,异常增强，以JSON的形式返回给客服端
  * @创建者: 独泪了无痕--duleilewuhen@sina.com
  * @创建时间: 2018年12月12日 下午10:47:57
- * @版本: V1.0.3
+ * @版本: V1.0.4
  * @since: JDK 1.8
  */
 @Slf4j
 @RestControllerAdvice
 public class DefaultExceptionHandler {
 	/**
-	 * @方法描述:权限校验失败
+	 * @方法描述 : TODO(这里用一句话描述这个方法的作用)
+	 */
+	@ExceptionHandler(UnknownAccountException.class)
+	public RestResult UnknownAccountExceptionHandle(UnknownAccountException uae) {
+		log.error(uae.getMessage(), uae);
+		SystemLogHelper.expLog(uae);
+		return RestResult.error(uae.getMessage());
+	}
+
+	/**
+	 * @方法描述 : TODO(这里用一句话描述这个方法的作用)
+	 */
+	@ExceptionHandler(IncorrectCredentialsException.class)
+	public RestResult IncorrectCredentialsExceptionHandle(IncorrectCredentialsException ice) {
+		log.error(ice.getMessage(), ice);
+		SystemLogHelper.expLog(ice);
+		return RestResult.error(ice.getMessage());
+	}
+
+	/**
+	 * @方法描述 : TODO(这里用一句话描述这个方法的作用)
+	 */
+	@ExceptionHandler(LockedAccountException.class)
+	public RestResult LockedAccountExceptionHandle(LockedAccountException lae) {
+		log.error(lae.getMessage(), lae);
+		SystemLogHelper.expLog(lae);
+		return RestResult.error(lae.getMessage());
+	}
+
+	/**
+	 * @方法描述 : TODO(这里用一句话描述这个方法的作用)
 	 */
 	@ExceptionHandler(AuthorizationException.class)
-	public RestResult handleAuthorizationException(AuthorizationException e) {
-		log.error(e.getMessage(), e);
-		SystemLogHelper.expLog(e);
-		return RestResult.error("您没有数据的权限，请联系管理员添加");
+	public RestResult handleAuthorizationExceptionHandle(AuthorizationException ae) {
+		log.error(ae.getMessage(), ae);
+		SystemLogHelper.expLog(ae);
+		return RestResult.error(ae.getMessage());
+	}
+
+	/**
+	 * @方法描述 : TODO(这里用一句话描述这个方法的作用)
+	 */
+	@ExceptionHandler(DisabledAccountException.class)
+	public RestResult DisabledAccountExceptionHandle(DisabledAccountException dae) {
+		log.error(dae.getMessage(), dae);
+		SystemLogHelper.expLog(dae);
+		return RestResult.error(dae.getMessage());
 	}
 
 	/**
@@ -76,13 +119,18 @@ public class DefaultExceptionHandler {
 		return RestResult.error(400, "缺少请求参数");
 	}
 
-	// 数组越界异常
+	/**
+	 * @方法描述 : 数组越界异常
+	 */
 	@ExceptionHandler(IndexOutOfBoundsException.class)
 	public RestResult indexOutOfBoundsExceptionHandler(IndexOutOfBoundsException ex) {
 		SystemLogHelper.expLog(ex);
 		return RestResult.error(1005, ex.getMessage());
 	}
 
+	/**
+	 * @方法描述 : TODO(这里用一句话描述这个方法的作用)
+	 */
 	@ExceptionHandler(HttpMessageNotReadableException.class)
 	public RestResult handleHttpMessageNotReadableExceptionHandler(
 			HttpMessageNotReadableException e) {
@@ -90,12 +138,9 @@ public class DefaultExceptionHandler {
 		return RestResult.error(400, "缺少请求参数");
 	}
 
-	@ExceptionHandler(UserNotExistsException.class)
-	public RestResult userNotExistExceptionHandler(UserNotExistsException ex) {
-		SystemLogHelper.expLog(ex);
-		return RestResult.error(ex.getMessage());
-	}
-
+	/**
+	 * @方法描述 : TODO(这里用一句话描述这个方法的作用)
+	 */
 	@ExceptionHandler({ ConversionNotSupportedException.class,
 			HttpMessageNotWritableException.class })
 	public RestResult server500(Exception e) {
@@ -104,14 +149,18 @@ public class DefaultExceptionHandler {
 		return RestResult.error(500, "运行时异常:" + e.getMessage());
 	}
 
-	// 406错误
+	/**
+	 * @方法描述 : 406错误
+	 */
 	@ExceptionHandler({ HttpMediaTypeNotAcceptableException.class })
 	public RestResult request406(Exception e) {
 		SystemLogHelper.expLog(e);
 		return RestResult.error(406, null);
 	}
 
-	// 非法参数
+	/**
+	 * @方法描述 : 非法参数
+	 */
 	@ExceptionHandler(MethodArgumentNotValidException.class)
 	public RestResult methodArgumentNotValidExceptionHandler(MethodArgumentNotValidException mane) {
 		SystemLogHelper.expLog(mane);
