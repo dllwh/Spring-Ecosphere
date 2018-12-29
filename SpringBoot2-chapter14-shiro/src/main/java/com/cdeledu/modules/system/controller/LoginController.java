154e761c707752e393e7853037bfe6dc7dce2f58
独泪了无痕 <8423799+dllwh@users.noreply.github.com>
2018-12-29 14:23:47 +0800

    2018年最后一天班，心烦

--- SpringBoot2-chapter14-shiro/src/main/java/com/cdeledu/modules/system/controller/LoginController.java
+++ SpringBoot2-chapter14-shiro/src/main/java/com/cdeledu/modules/system/controller/LoginController.java
@@ -1,10 +1,15 @@
 package com.cdeledu.modules.system.controller;
 
+import org.apache.commons.lang3.StringUtils;
+import org.apache.shiro.authc.AuthenticationException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cdeledu.common.RestResult;
+import com.cdeledu.framework.shiro.service.LoginService;
 
 import io.swagger.annotations.Api;
 
@@ -16,13 +21,17 @@ import io.swagger.annotations.Api;
  * @类描述:登录控制器
  * @创建者: 独泪了无痕--duleilewuhen@sina.com
  * @创建时间: 2018年12月9日 下午7:23:43
- * @版本: V1.0.1
+ * @版本: V1.0.2
  * @since: JDK 1.8
  */
 @RestController
 @Api(tags = "登录验证")
 @RequestMapping("login")
 public class LoginController {
+
+	@Autowired
+	private LoginService loginService;
+
 	/**
 	 * @方法描述:登录
 	 * @param username
@@ -32,12 +41,32 @@ public class LoginController {
 	 * @param rememberMe
 	 *            是否记住我
 	 * @return
+	 * @throws MissingServletRequestParameterException
 	 */
 	@PostMapping(value = "ajaxLogin")
-	public RestResult ajaxLogin(String username, String password, Boolean rememberMe) {
-		return RestResult.success();
+	public RestResult ajaxLogin(String userName, String password, Boolean rememberMe)
+			throws Exception {
+
+		if (StringUtils.isBlank(userName)) { // 用户名为空 错误
+			throw new MissingServletRequestParameterException("userName", "String");
+		}
+
+		if (StringUtils.isBlank(password)) { // 密码为空 错误
+			throw new MissingServletRequestParameterException("password", "String");
+		}
+
+		try {
+			loginService.login(userName, password);
+		} catch (AuthenticationException e) {
+			String msg = "用户或密码错误";
+			if (StringUtils.isNotEmpty(e.getMessage())) {
+				msg = e.getMessage();
+			}
+			return RestResult.error(msg);
+		}
+		return RestResult.success("登录成功");
 	}
-	
+
 	/**
 	 * @方法描述:登录验证
 	 * @return
@@ -46,7 +75,7 @@ public class LoginController {
 	public RestResult checkuser() {
 		return RestResult.success();
 	}
-	
+
 	/**
 	 * @方法描述:退出
 	 * @return
