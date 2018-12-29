bc42b2946b2170f2b5f059a574277f65efadb1c9
独泪了无痕 <8423799+dllwh@users.noreply.github.com>
2018-12-29 14:10:58 +0800

    Update SysLogMapper.java

--- SpringBoot2-chapter14-shiro/src/main/java/com/cdeledu/modules/monitor/syslog/mapper/SysLogMapper.java
+++ SpringBoot2-chapter14-shiro/src/main/java/com/cdeledu/modules/monitor/syslog/mapper/SysLogMapper.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
 
 import com.cdeledu.framework.model.LoggerEntity;
+import com.cdeledu.modules.monitor.syslog.domain.SysLoginLog;
 
 /**
  * 把今天最好的表现当作明天最新的起点．．～
@@ -14,7 +15,7 @@ import com.cdeledu.framework.model.LoggerEntity;
  * @类描述:
  * @创建者: 独泪了无痕--duleilewuhen@sina.com
  * @创建时间: 2018年12月9日 下午7:13:05
- * @版本: V1.0.1
+ * @版本: V1.0.2
  * @since: JDK 1.8
  */
 @Mapper
@@ -23,28 +24,28 @@ public interface SysLogMapper {
 	 * 访问异常日志
 	 */
 	int insertExpLog(LoggerEntity expLog);
-	
+
 	List<LoggerEntity> getExpLogList(LoggerEntity expLog);
-	
+
 	int countExpLog(LoggerEntity expLog);
-	
+
 	/**
 	 * 登录日志
 	 */
-	int insertLoginLog(LoggerEntity expLog);
-	
-	List<LoggerEntity> getLoginLogList(LoggerEntity expLog);
-	
-	List<LoggerEntity> getLoginLogByUserID(int userId);
-	
-	int countLoginLog(LoggerEntity expLog);
-	
+	int insertLoginLog(SysLoginLog loginLog);
+
+	List<SysLoginLog> getLoginLogList(SysLoginLog loginLog);
+
+	List<SysLoginLog> getLoginLogByUserName(String userName);
+
+	int countLoginLog(SysLoginLog loginLog);
+
 	/**
 	 * 操作日志
 	 */
 	int insertOperateLog(LoggerEntity operateLog);
-	
+
 	List<LoggerEntity> getOperateLogList(LoggerEntity expLog);
-	
+
 	int countOperateLog(LoggerEntity expLog);
 }
