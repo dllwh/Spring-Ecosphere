29e8d9007c90f1ec424bb76e59f284af4fb22e23
独泪了无痕 <8423799+dllwh@users.noreply.github.com>
2018-12-29 20:23:50 +0800

    密码服务

--- SpringBoot2-chapter14-shiro/src/main/java/com/cdeledu/framework/shiro/service/PasswordService.java
+++ SpringBoot2-chapter14-shiro/src/main/java/com/cdeledu/framework/shiro/service/PasswordService.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Component;
  * @类描述: 密码服务
  * @创建者: 独泪了无痕--duleilewuhen@sina.com
  * @创建时间: 2018年12月9日 下午7:41:11
- * @版本: V1.0.1
+ * @版本: V1.0.2
  * @since: JDK 1.8
  */
 @Component
@@ -19,11 +19,11 @@ public class PasswordService {
 	/** 密钥 */
 	private static final String PRIVATE_KEY = "237c038cd985c0f75e38756af2d54de9";
 
-	public String encryptPassword(String username, String password) {
-		return new Md5Hash(username + password + PRIVATE_KEY).toHex();
+	public String encryptPassword(String userName, String password) {
+		return encryptPassword(userName, password, PRIVATE_KEY);
 	}
 
-	public String encryptPassword(String username, String password, String salt) {
-		return new Md5Hash(username + password + salt).toHex();
+	public String encryptPassword(String userName, String password, String salt) {
+		return new Md5Hash(userName + password + salt).toHex();
 	}
 }
