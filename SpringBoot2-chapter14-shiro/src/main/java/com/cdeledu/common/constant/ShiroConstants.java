9c5bdd9e2acdf71160040e638c8be027ae2dfb79
独泪了无痕 <8423799+dllwh@users.noreply.github.com>
2018-12-30 14:06:46 +0800

    Shiro通用常量

--- SpringBoot2-chapter14-shiro/src/main/java/com/cdeledu/common/constant/ShiroConstants.java
+++ SpringBoot2-chapter14-shiro/src/main/java/com/cdeledu/common/constant/ShiroConstants.java
@@ -9,7 +9,7 @@ package com.cdeledu.common.constant;
  * @创建者: 皇族灬战狼
  * @联系方式: duleilewuhen@sina.com
  * @创建时间: 2018年12月14日 下午7:04:05
- * @版本: V 0.1
+ * @版本: V 1.0.2
  * @since: JDK 1.7
  */
 public interface ShiroConstants {
@@ -17,4 +17,12 @@ public interface ShiroConstants {
 	public static final String	CURRENT_USER		= "currentUser";
 	/** 用户名 */
 	public static final String	CURRENT_USERNAME	= "username";
+	/** 登录请求 */
+	public final static String	LOGIN_ACTION		= "/login/ajaxLogin";
+	/** 登录验证 */
+	public final static String	LOGIN_CHECK			= "/login?checkuser";
+	/** 踢出登录提示 */
+	public final static String	KICKED_OUT			= "/login/kickout";
+	/** 没有权限提醒 */
+	public final static String	UNAUTHORIZED		= "/error/unauth";
 }
