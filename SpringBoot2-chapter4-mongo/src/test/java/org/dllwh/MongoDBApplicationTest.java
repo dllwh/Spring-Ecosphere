f80a61d777e1b2b3773a25fc21c673c4ab7623ed
独泪了无痕 <duleilewuhen@sina.com>
2019-01-28 23:52:37 +0800

    优化单元测试

--- SpringBoot2-chapter4-mongo/src/test/java/org/dllwh/MongoDBApplicationTest.java
+++ SpringBoot2-chapter4-mongo/src/test/java/org/dllwh/MongoDBApplicationTest.java
@@ -6,11 +6,12 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringBootTest(classes = MongoSpringBootApplication.class)
+@SpringBootTest(webEnvironment=WebEnvironment.RANDOM_PORT)
 public class MongoDBApplicationTest {
 	/** ----------------------------------------------------- Fields start */
 
