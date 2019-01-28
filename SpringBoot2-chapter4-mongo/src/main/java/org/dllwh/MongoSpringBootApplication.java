05d684647a5a31b4edd5ebd93c13f3011d18e5df
独泪了无痕 <duleilewuhen@sina.com>
2019-01-28 23:52:23 +0800

    增加项目启动过滤类

--- SpringBoot2-chapter4-mongo/src/main/java/org/dllwh/MongoSpringBootApplication.java
+++ SpringBoot2-chapter4-mongo/src/main/java/org/dllwh/MongoSpringBootApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 // 该注解指定项目为springboot，由此类当作程序入口自动装配 web 依赖的环境
+// @SpringBootApplication(exclude = { MongoAutoConfiguration.class, MongoDataAutoConfiguration.class })
 @SpringBootApplication
 public class MongoSpringBootApplication {
 	public static void main(String[] args) {
