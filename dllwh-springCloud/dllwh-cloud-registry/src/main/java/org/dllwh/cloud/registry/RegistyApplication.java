713b03cf09a09bdf44c490c1125698636c9357ea
独泪了无痕 <duleilewuhen@sina.com>
2019-02-23 11:40:25 +0800

    <!-- 引入 Eureka Server 依赖 -->

--- dllwh-springCloud/dllwh-cloud-registry/src/main/java/org/dllwh/cloud/registry/RegistyApplication.java
+++ dllwh-springCloud/dllwh-cloud-registry/src/main/java/org/dllwh/cloud/registry/RegistyApplication.java
@@ -2,8 +2,10 @@ package org.dllwh.cloud.registry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class RegistyApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(RegistyApplication.class, args);
