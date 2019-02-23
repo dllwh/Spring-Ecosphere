7b6b69691447dd901e160022b2fbdcf7edbb5977
独泪了无痕 <duleilewuhen@sina.com>
2019-02-23 11:33:44 +0800

    基于Euraka构建服务注册中心，负责服务注册于发现

---
+++ dllwh-springCloud/dllwh-cloud-registry/src/main/java/org/dllwh/cloud/registry/RegistyApplication.java
@@ -0,0 +1,11 @@
+package org.dllwh.cloud.registry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistyApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(RegistyApplication.class, args);
+	}
+}
