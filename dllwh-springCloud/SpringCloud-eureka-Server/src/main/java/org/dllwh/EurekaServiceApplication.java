6f17ee11e454f2354607924d3ae2eba9925f97fa
独泪了无痕 <duleilewuhen@sina.com>
2019-02-24 23:57:28 +0800

    玩转SpringCloud（F版本） 一．服务的注册与发现（Eureka）

--- dllwh-springCloud/SpringCloud-eureka-Server/src/main/java/org/dllwh/EurekaServiceApplication.java
+++ dllwh-springCloud/SpringCloud-eureka-Server/src/main/java/org/dllwh/EurekaServiceApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableEurekaServer
+@EnableEurekaServer // 表明这是一个EurekaServer（服务注册中心）
 public class EurekaServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaServiceApplication.class, args);
--- dllwh-springCloud/SpringCloud-eureka-provider/src/main/java/org/dllwh/EurekaProviderApplication.java
+++ dllwh-springCloud/SpringCloud-eureka-provider/src/main/java/org/dllwh/EurekaProviderApplication.java
@@ -4,8 +4,20 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+/**
+ * 
+ * 把今天最好的表现当作明天最新的起点．．～
+ *
+ * Today the best performance as tomorrow newest starter!
+ *
+ * @类描述: 基于spring-cloud-netflix，只能为eureka作用
+ * @创建者: 独泪了无痕--duleilewuhen@sina.com
+ * @创建时间: 2019年2月24日 下午11:01:53
+ * @版本: V1.0.1
+ * @since: JDK 1.8
+ */
 @SpringBootApplication
-@EnableEurekaClient
+@EnableEurekaClient // 表明这是一个EurekaClient（服务提供者）
 public class EurekaProviderApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaProviderApplication.class, args);
