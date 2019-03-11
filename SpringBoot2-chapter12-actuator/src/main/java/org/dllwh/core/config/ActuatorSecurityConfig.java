63848ec2ae58e1dc4f687b2cec205b067b5aa434
独泪了无痕 <duleilewuhen@sina.com>
2019-03-11 22:42:58 +0800

    安全认证

--- SpringBoot2-chapter12-actuator/src/main/java/org/dllwh/core/config/ActuatorSecurityConfig.java
+++ SpringBoot2-chapter12-actuator/src/main/java/org/dllwh/core/config/ActuatorSecurityConfig.java
@@ -1,30 +1,30 @@
-//package org.dllwh.core.config;
-//
-//import org.apache.commons.lang3.StringUtils;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.core.env.Environment;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-//
-//@Configuration
-//@EnableWebSecurity
-//public class ActuatorSecurityConfig extends WebSecurityConfigurerAdapter {
-//	@Autowired
-//	Environment env;
-//
-//	/**
-//	 * 只对EndPoint的访问加验证
-//	 */
-//	@Override
-//	protected void configure(HttpSecurity http) throws Exception {
-//		String contextPath = env.getProperty("management.endpoints.web.base-path");
-//		if (StringUtils.isEmpty(contextPath)) {
-//			contextPath = "";
-//		}
-//		http.csrf().disable();
-//		http.authorizeRequests().antMatchers("/**" + contextPath + "/**").authenticated().anyRequest()
-//				.permitAll().and().httpBasic();
-//	}
-//}
+package org.dllwh.core.config;
+
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class ActuatorSecurityConfig extends WebSecurityConfigurerAdapter {
+	@Autowired
+	Environment env;
+
+	/**
+	 * 只对EndPoint的访问加验证
+	 */
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		String contextPath = env.getProperty("management.endpoints.web.base-path");
+		if (StringUtils.isEmpty(contextPath)) {
+			contextPath = "";
+		}
+		http.csrf().disable();
+		http.authorizeRequests().antMatchers("/**" + contextPath + "/**").authenticated().anyRequest()
+				.permitAll().and().httpBasic();
+	}
+}
