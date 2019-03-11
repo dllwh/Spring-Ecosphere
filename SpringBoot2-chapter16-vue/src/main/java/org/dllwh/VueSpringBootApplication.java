c829845ef1642ea9389dffe0c7986e8a710e7af1
独泪了无痕 <duleilewuhen@sina.com>
2019-03-11 22:57:48 +0800

    初始化

--- SpringBoot2-chapter16-vue/src/main/java/org/dllwh/VueSpringBootApplication.java
+++ SpringBoot2-chapter16-vue/src/main/java/org/dllwh/VueSpringBootApplication.java
@@ -1,5 +1,11 @@
 package org.dllwh;
 
-public class VueSpringBootApplication {
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
+public class VueSpringBootApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(VueSpringBootApplication.class, args);
+	}
 }
