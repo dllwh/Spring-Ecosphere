e806b9083be5d737a86446973e18095766738aa7
独泪了无痕 <duleilewuhen@sina.com>
2019-08-10 01:40:31 +0800

    SpringBoot 发送邮件

---
+++ dllwh-SpringBoot2/SpringBoot2-chapter21-mail/src/main/java/org/dllwh/mail/MailApplication.java
@@ -0,0 +1,22 @@
+package org.dllwh.mail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 把今天最好的表现当作明天最新的起点．．～
+ *
+ * Today the best performance as tomorrow newest starter!
+ *
+ * @类描述: 项目启动类
+ * @创建者: 独泪了无痕--duleilewuhen@sina.com
+ * @创建时间: Aug 9, 2019 10:04:20 PM
+ * @版本: V1.0.1
+ * @since: JDK 1.8
+ */
+@SpringBootApplication
+public class MailApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(MailApplication.class, args);
+	}
+}
