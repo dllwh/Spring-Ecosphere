c4a842e07636c03c23552e0112690cea2793272e
独泪了无痕 <duleilewuhen@sina.com>
2019-10-13 02:23:29 +0800

    SpringBoot+SpringMVC+MybatisPlus框架整合

---
+++ dllwh-SpringBoot2/SpringBoot2-chapter21-mybatisPlus/src/main/java/org/dllwh/mybatisPlus/MybatisPlusApplication.java
@@ -0,0 +1,25 @@
+package org.dllwh.mybatisPlus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * 把今天最好的表现当作明天最新的起点．．～
+ *
+ * Today the best performance as tomorrow newest starter!
+ *
+ * @类描述: 项目启动类
+ * @创建者: 独泪了无痕--duleilewuhen@sina.com
+ * @创建时间: 2019-10-13 2:18:26 AM
+ * @版本: V1.0.1
+ * @since: JDK 1.8
+ */
+@SpringBootApplication
+@EnableSwagger2 // 开启 swagger2 功能
+public class MybatisPlusApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(MybatisPlusApplication.class, args);
+	}
+}
