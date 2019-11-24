884e3b4aa04adfe2ff3592a48f9a9ee158c3108d
独泪了无痕 <duleilewuhen@sina.com>
2019-11-25 01:08:22 +0800

    Mybatis-plus 代码生成器 测试 V 1.0.1

---
+++ dllwh-SpringBoot2/SpringBoot2-chapter21-mybatisPlus/src/main/test/org/dllwh/mybatisplus/MyBatisPlusGeneratorTest.java
@@ -0,0 +1,28 @@
+package org.dllwh.mybatisplus;
+
+import org.dllwh.mybatisplus.generator.MyBatisPlusGenerator;
+
+/**
+ * 把今天最好的表现当作明天最新的起点．．～
+ *
+ * Today the best performance as tomorrow newest starter!
+ *
+ * @类描述: Mybatis-plus 代码生成器 测试
+ * @创建者: <a href="mailto:duleilewuhen@sina.com">独泪了无痕</a>
+ * @创建时间: 2019-11-25 12:42:15 AM
+ * @版本: V 1.0.1
+ * @since: JDK 1.8
+ */
+public class MyBatisPlusGeneratorTest {
+	private static String moduleName = "mybatisplusgenerator";
+	private static String dbType = "MYSQL";
+	private static String driverName = "com.mysql.jdbc.Driver";
+	private static String url = "jdbc:mysql://rm-duleilewuhen.mysql.rds.aliyuncs.com:3306/spring?useUnicode=true&characterEncoding=UTF-8&useSSL=true";
+	private static String userName = "spring";
+	private static String password = "spring";
+
+	public static void main(String[] args) {
+		new MyBatisPlusGenerator(null, null, moduleName, null, dbType, driverName, url, userName, password).codeAutoGenerator("crawler_china_area");
+				
+	}
+}
