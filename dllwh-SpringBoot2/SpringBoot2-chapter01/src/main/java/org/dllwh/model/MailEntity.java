0aca672470ede49ecd538597d41de245327128c9
独泪了无痕 <duleilewuhen@sina.com>
2019-12-18 21:51:44 +0800

    根据Java命名规范修改不符合规范的代码

--- dllwh-SpringBoot2/SpringBoot2-chapter01/src/main/java/org/dllwh/model/MailEntity.java
+++ dllwh-SpringBoot2/SpringBoot2-chapter01/src/main/java/org/dllwh/model/MailEntity.java
@@ -20,13 +20,21 @@ import lombok.Data;
  */
 @Data
 public class MailEntity implements Serializable {
-	private static final long	serialVersionUID	= 1L;
-	private String				smtpService;							// 此处填写SMTP服务器
-	private String				smtpPort;								// 设置端口号
-	private String				fromMailAddress;						// 设置发送邮箱
-	private String				fromMailStmpPwd;						// 设置发送邮箱的STMP口令
-	private String				title;									// 设置邮件标题
-	private String				content;								// 设置邮件内容
-	private String				contentType;							// 内容格式（默认采用html）
-	private List<String>		list				= new ArrayList<>();// 接受邮件地址集合
+	private static final long serialVersionUID = 2444484152001143668L;
+	/** 此处填写SMTP服务器 */
+	private String            smtpService;
+	/** 设置端口号 */
+	private String            smtpPort;
+	/** 设置发送邮箱 */
+	private String            fromMailAddress;
+	/** 设置发送邮箱的STMP口令 */
+	private String            fromMailStmpPwd;
+	/** 设置邮件标题 */
+	private String            title;
+	/** 设置邮件内容 */
+	private String            content;
+	/** 内容格式（默认采用html） */
+	private String            contentType;
+	/** 接受邮件地址集合 */
+	private List<String>      list             = new ArrayList<>();
 }
--- dllwh-SpringBoot2/SpringBoot2-chapter01/src/main/java/org/dllwh/model/rbac/SysUserRoleDTO.java
+++ dllwh-SpringBoot2/SpringBoot2-chapter01/src/main/java/org/dllwh/model/rbac/SysUserRoleDTO.java
@@ -1,13 +1,23 @@
 package org.dllwh.model.rbac;
 
+import java.io.Serializable;
+
 import lombok.Data;
 
 @Data
-public class SysUserRoleDTO {
-	private Integer	userId;		// 管理员id
-	private String	userName;	// 登录名
-	private Integer	userCount;	// 用户个数
-	private Integer	roleId;		// 角色id
-	private String	roleName;	// 角色名称
-	private String	roleCode;	// 角色编码
+public class SysUserRoleDTO implements Serializable {
+
+	private static final long serialVersionUID = -747625094926250378L;
+	/** 管理员id */
+	private Integer           userId;
+	/** 登录名 */
+	private String            userName;
+	/** 用户个数 */
+	private Integer           userCount;
+	/** 角色id */
+	private Integer           roleId;
+	/** 角色名称 */
+	private String            roleName;
+	/** 角色编码 */
+	private String            roleCode;
 }
