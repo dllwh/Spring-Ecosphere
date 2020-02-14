09b94a26a4b4279a14ebd7f6edf88fb9f06f7aa2
独泪了无痕 <duleilewuhen@sina.com>
2020-02-14 21:05:37 +0800

    修改备注说明

--- dllwh-SpringBoot2/SpringBoot2-chapter05-websocket/src/main/java/org/dllwh/WebsocketApplication.java
+++ dllwh-SpringBoot2/SpringBoot2-chapter05-websocket/src/main/java/org/dllwh/WebsocketApplication.java
@@ -9,7 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  *
  * Today the best performance as tomorrow newest starter!
  *
- * @类描述: 该注解指定项目为springboot，由此类当作程序入口自动装配 web 依赖的环境
+ * @类描述: websocket服务器入口类。由此类当作程序入口自动装配 web 依赖的环境
  * @author: <a href="mailto:duleilewuhen@sina.com">独泪了无痕</a>
  * @创建时间: 2020-01-12
  * @版本: V 1.0.1
