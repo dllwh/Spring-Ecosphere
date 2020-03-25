6b896b05822d0c8b59069092c03555dc3f6a64cd
独泪了无痕 <duleilewuhen@sina.com>
2020-03-26 00:39:43 +0800

    合并maser分支代码

--- dllwh-SpringBoot2/SpringBoot2-chapter03-redis/src/main/java/org/dllwh/RedisSpringBootApplication.java
+++ dllwh-SpringBoot2/SpringBoot2-chapter03-redis/src/main/java/org/dllwh/RedisSpringBootApplication.java
@@ -2,12 +2,10 @@ package org.dllwh;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
-//该注解指定项目为springboot，由此类当作程序入口自动装配 web 依赖的环境
-@SpringBootApplication
-@EnableSwagger2
+@SpringBootApplication // 该注解指定项目为SpringBoot，由此类当作程序入口自动装配 web 依赖的环境
+@EnableCaching // 注解配置启用缓存，自动配置配置文件的配置信息进行条件注入缓存所需实例
 public class RedisSpringBootApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(RedisSpringBootApplication.class, args);
