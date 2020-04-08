64837d1ef22560ca7c5684580c2f978cf3d30e63
独泪了无痕 <duleilewuhen@sina.com>
2020-04-08 23:39:34 +0800

    SpringBoot MyBatis

--- dllwh-SpringBoot2/SpringBoot2-chapter01/src/test/java/org/dllwh/MyBatisApplicationTest.java
+++ dllwh-SpringBoot2/SpringBoot2-chapter01/src/test/java/org/dllwh/MyBatisApplicationTest.java
@@ -1,5 +1,6 @@
 package org.dllwh;
 
+import org.dllwh.service.RoleService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,16 +9,11 @@ import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
 
-import org.dllwh.service.RoleService;
-
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class MyBatisApplicationTest {
-	/** ----------------------------------------------------- Fields start */
 	@Autowired
 	private RoleService roleService;
-
-	/** ----------------------------------------------------- Fields end */
 
 	@Test
 	@Transactional
