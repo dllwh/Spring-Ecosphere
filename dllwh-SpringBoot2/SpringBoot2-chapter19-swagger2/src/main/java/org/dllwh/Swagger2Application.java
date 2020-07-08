803b29226b9682568e2c685737ba450c5d5ea769
独泪了无痕 <duleilewuhen@sina.com>
2020-07-09 01:35:33 +0800

    类名不符合开发规范

---
+++ dllwh-SpringBoot2/SpringBoot2-chapter19-swagger2/src/main/java/org/dllwh/Swagger2Application.java
@@ -0,0 +1,21 @@
+package org.dllwh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 把今天最好的表现当作明天最新的起点．．～
+ * <p>
+ * Today the best performance as tomorrow newest starter!
+ *
+ * @类描述: 项目启动类
+ * @author: <a href="mailto:duleilewuhen@sina.com">独泪了无痕</a>
+ * @版本: V 1.0.1
+ * @since: JDK 1.8
+ */
+@SpringBootApplication
+public class Swagger2Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Swagger2Application.class, args);
+    }
+}
