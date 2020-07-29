5f7126cbbe50afdf122ddd5d7a3845e1745c6171
独泪了无痕 <duleilewuhen@sina.com>
2020-07-29 21:56:19 +0800

    删除错误的代码

--- dllwh-SpringBoot2/SpringBoot2-chapter20-thymeleaf/src/main/java/org/dllwh/thymeleaf/ThymeleafApplication.java
+++ dllwh-SpringBoot2/SpringBoot2-chapter20-thymeleaf/src/main/java/org/dllwh/thymeleaf/ThymeleafApplication.java
@@ -1,7 +1,5 @@
 package org.dllwh.thymeleaf;
 
-import io.github.yedaxia.apidocs.Docs;
-import io.github.yedaxia.apidocs.DocsConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -21,19 +19,5 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ThymeleafApplication {
     public static void main(String[] args) {
         SpringApplication.run(ThymeleafApplication.class, args);
-
-        DocsConfig config = new DocsConfig();
-        // 项目根目录
-        config.setProjectPath("/Users/dllwh/DevGit/Spring-Ecosphere/dllwh-SpringBoot2/SpringBoot2-chapter20-thymeleaf");
-        // 项目名称
-        config.setProjectName("ProjectName");
-        // 声明该API的版本
-        config.setApiVersion("V1.0");
-        // 生成API 文档所在目录
-        config.setDocsPath("/Users/dllwh/DevGit/Spring-Ecosphere/dllwh-SpringBoot2/SpringBoot2-chapter20-thymeleaf/");
-        // 配置自动生成
-        config.setAutoGenerate(Boolean.TRUE);
-        // 执行生成文档
-        Docs.buildHtmlDocs(config);
     }
 }
