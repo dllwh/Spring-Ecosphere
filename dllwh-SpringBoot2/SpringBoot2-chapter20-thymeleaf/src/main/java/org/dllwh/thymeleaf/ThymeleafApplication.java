0253b26e00c14429dc31a8ae00ed2c69cbdbd77c
独泪了无痕 <duleilewuhen@sina.com>
2020-07-29 20:57:39 +0800

    删除不符合规则的文件

---
+++ dllwh-SpringBoot2/SpringBoot2-chapter20-thymeleaf/src/main/java/org/dllwh/thymeleaf/ThymeleafApplication.java
@@ -0,0 +1,24 @@
+package org.dllwh.thymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 把今天最好的表现当作明天最新的起点．．～
+ * <p>
+ * Today the best performance as tomorrow newest starter!
+ *
+ * @类描述: TODO(这里用一句话描述这个类的作用)
+ * @author: <a href="mailto:duleilewuhen@sina.com">独泪了无痕</a>
+ * @创建时间: 2020/7/29 20:55
+ * @版本: V 1.0.1
+ * @since: JDK 1.8
+ * @see <a href="">TODO(连接内容简介)</a>
+ */
+@SpringBootApplication
+
+public class ThymeleafApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ThymeleafApplication.class, args);
+    }
+}
