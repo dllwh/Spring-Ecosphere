636221b02ba98019155a91ded4e7f51a034f98b0
独泪了无痕 <duleilewuhen@sina.com>
2020-10-19 08:52:09 +0800

    SpringBoot  整合AOP

--- dllwh-SpringBoot2/SpringBoot2-chapter23-Aop/src/main/java/org/dllwh/aop/model/WebLog.java
+++ dllwh-SpringBoot2/SpringBoot2-chapter23-Aop/src/main/java/org/dllwh/aop/model/WebLog.java
@@ -1,6 +1,9 @@
 package org.dllwh.aop.model;
 
+import java.io.Serializable;
+
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /**
  * 把今天最好的表现当作明天最新的起点．．～
@@ -16,23 +19,42 @@ import lombok.Data;
  *
  */
 @Data
-public class WebLog {
-	/** 操作描述 */
-	private String description;
-	/** 操作用户 */
-	private String userName;
-	/** IP地址 */
-	private String ipAddress;
+@NoArgsConstructor
+public class WebLog implements Serializable {
+
+	private static final long serialVersionUID = 5132663133670672913L;
+
+	/** 主键 */
+	private Integer id;
+	/** 操作版本号 */
+	private String operateVer;
+
+	/** 操作模块 */
+	private String operateModel;
+	/** 操作类型 */
+	private String operateType;
+	/** 操作说明 */
+	private String operateDesc;
+
+	/** 操作者ID */
+	private String operateUserId;
+	/** 操作者名称 */
+	private String operateUserName;
+
 	/** 目标类名 */
-	private String className;
+	private String operateClass;
 	/** 目标方法 */
-	private String methodName;
+	private String operateMethod;
+
 	/** 操作时间 */
 	private Long startTime;
 	/** 消耗时间(单位：毫秒) */
 	private Long spendTime;
+
+	/** IP地址 */
+	private String ipAddress;
 	/** 请求方式(请求类型) */
-	private String method;
+	private String requestType;
 	/** 根路径 */
 	private String basePath;
 	/** URI */
@@ -42,13 +64,13 @@ public class WebLog {
 	/** 请求参数 */
 	private Object parameter;
 	/** 请求返回的结果 */
-	private Object result;
-	/** 日志类型 */
-	private int logType;
+	private String result;
+	/** 日志类型(info:操作日志,error:错误) */
+	private String logType = "info";
+	
 	/** 异常详情 */
-	private String exceptionDetail = "";
+	private String excepDetail = "";
 	/** 异常名称 */
-	private String exceptionName = "";
-	/** 操作模块 */
-	private String operMudel;
+	private String excepName = "";
+
 }
